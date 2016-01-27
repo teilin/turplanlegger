e9b3c3f2c5db6d58ad51990e7aca61b03a177069
Teis Lindemark <teis.lindemark@gmail.com>
2016-01-27 23:28:19 +0100

    Starting to play around with adding services to aspnet core web site

--- Services/ITurbasenService.cs
+++ Services/ITurbasenService.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using turplanlegger.Models;
 
 namespace turplanlegger.Services
 {
     public interface ITurbasenService
     {
-        Task GetTurer();
+        Task<IList<Tur>> GetTurer();
     }
 }
--- Services/TurbasenService.cs
+++ Services/TurbasenService.cs
@@ -4,18 +4,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using turplanlegger.Models;
+using Newtonsoft.Json;
 
 namespace turplanlegger.Services
 {
     public class TurbasenService : ITurbasenService
     {
-        public async Task GetTurer()
+        public async Task<IList<Tur>> GetTurer()
         {
+            var retList = new List<Tur>();
             using(var client = new HttpClient())
             {
                 var response = await client.GetAsync("");
+                if(response.IsSuccessStatusCode)
+                {
+                    retList = Task.Factory.StartNew(() => 
+                        JsonConvert.DeserializeObject<List<Tur>>(response.Content.ToString())
+                    ).Result;
+                }
+                return retList;
             }
-            throw new NotImplementedException();
         }
     }
 }
